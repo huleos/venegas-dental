37f80985e7294ce369b190c72835c0aa3e12a4ac
Julio Cesar Montenegro <creativo@686studio.com>
2017-07-28 11:05:49 -0700

    update header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -16,9 +16,10 @@
       <div class="c-header--logo">
         <img src="<?= url('assets/images/logo.png') ?>" alt="">
       </div>
-      <nav class="c-header--navigation o-flex o-flex--wrap o-flex--spaced">
+      <nav class="c-header--navigation o-flex o-flex--center o-flex--middle">
         <a href="" class="c-header--navigation__link">About</a>
         <a href="" class="c-header--navigation__link">Contact</a>
+        <a href="#" class="c-button c-button--primary c-header__cta">Call to action</a>
       </nav>
     </div>
   </div>
