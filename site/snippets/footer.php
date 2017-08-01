f24dc6d3ca18bf9069a1a1dde4d83d1c7850192e
Julio Cesar Montenegro <creativo@686studio.com>
2017-08-01 11:38:01 -0700

    added footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,3 +1,31 @@
+	<footer class="c-footer">
+		<div class="c-footer--newsletter">
+			<div class="o-container">
+  			<h3 class="c-footer--newsletter__title">Newsletter</h3>
+  			<p class="c-footer--newsletter__text">Join our newsletter and receive special offers</p>
+    	</div>
+    	<div class="o-container--sm">
+    		<div class="o-flex o-flex--center">
+	  			<form action="" class="c-footer--form">
+	  				<input type="text" placeholder="Enter your email address" class="c-form-input">
+	  				<input class="c-button c-button--primary c-button--large" type="submit" value="Subscribe">
+	  			</form>
+  			</div>
+    	</div>
+		</div>
+		<div class="c-footer--cr">
+			<div class="o-container">
+				<div class="o-flex o-flex--justify o-flex--middle">
+					<p>&copy; 2017 All Rights Reserved</p>
+					<nav class="c-footer--nav o-flex o-flex--center o-flex--middle">
+						<a href="" class="c-footer--nav__link">Terms & Conditions</a>
+						<a href="" class="c-footer--nav__link">Privacy Policy</a>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</footer>
+
 	<?= js('assets/js/bundle.js') ?>
 </body>
 </html>
