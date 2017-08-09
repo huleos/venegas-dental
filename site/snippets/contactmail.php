b2adcc9e57b17d81872a8d0c3fef3850feafe1c0
Julio Cesar Montenegro <creativo@686studio.com>
2017-08-09 12:45:14 -0700

    plantilla

--- site/snippets/contactmail.php
+++ site/snippets/contactmail.php
@@ -1,8 +1,13 @@
-<?= '<b>'.'You have received a new message, here are the details:'.'</b>' ?>
+You have received a new message, here are the details:
+----
 Name: <?= $fullName ?>
+----
 Email: <?= $emailAddress ?>
+----
 City of residence: <?= $cityOfResidence ?>
+----
 Contact number: <?= $contactNumber ?>
+----
 Message: <?= $message ?>
 
 
