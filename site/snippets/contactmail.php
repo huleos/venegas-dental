66cf3eba5967badc5a4b1f8aae28d1487cc29b20
Julio Cesar Montenegro <creativo@686studio.com>
2017-08-09 13:32:12 -0700

    form

--- site/snippets/contactmail.php
+++ site/snippets/contactmail.php
@@ -1,22 +1,15 @@
 You have received a new message, here are the details:
 ----
 Name: <?= $fullName ?>
+
+----
 Email: <?= $emailAddress ?>
+
+----
 City of residence: <?= $cityOfResidence ?>
+
+----
 Contact number: <?= $contactNumber ?>
+
+----
 Message: <?= $message ?>
-
-
-
-<?php
-    echo '<p>' . 'hola' . '<p>'."\n";
-?>
-
-<?php /*Do some PHP calculation or something*/ ?>
-     <table>
-         <tr>
-             <td>Name</td>
-             <td><?= $fullName ?></td>
-         </tr>
-     </table>
-?>
