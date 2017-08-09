b2a6d3b3c07517b43702d73a60990c7f3a32f980
Julio Cesar Montenegro <creativo@686studio.com>
2017-08-09 12:30:53 -0700

    experimentos con la plantilla de correo

--- site/snippets/contactmail.php
+++ site/snippets/contactmail.php
@@ -1,7 +1,16 @@
-You have received a new message, here are the details:
-<br>
-<b>Name:</b> <?= $fullName ?>
-<b>Email:</b> <?= $emailAddress ?>
-<b>City of residence:</b> <?= $cityOfResidence ?>
-<b>Contact number:</b> <?= $contactNumber ?>
-<b>Message:</b><br> <?= $message ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Email</title>
+</head>
+<body>
+	You have received a new message, here are the details:
+	<br>
+	<b>Name:</b> <?= $fullName ?><br>
+	<b>Email:</b> <?= $emailAddress ?><br>
+	<b>City of residence:</b> <?= $cityOfResidence ?><br>
+	<b>Contact number:</b> <?= $contactNumber ?><br>
+	<b>Message:</b><br> <?= $message ?>
+</body>
+</html>
