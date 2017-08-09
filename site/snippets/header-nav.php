7f50f904d0198dd4452957890e2ae46684494450
Julio Cesar Montenegro <creativo@686studio.com>
2017-08-09 16:38:46 -0700

    added info in header

--- site/snippets/header-nav.php
+++ site/snippets/header-nav.php
@@ -1,5 +1,6 @@
 <nav class="c-header--navigation o-flex o-flex--center o-flex--middle">
   <!-- <a href="" class="c-header--navigation__link">About</a>
   <a href="" class="c-header--navigation__link">Contact</a> -->
-  <a href="#" class="c-button c-button--primary c-header__cta">Call to action</a>
+  <div class="c-header__tel">Need Help? Call us Now <a href="tel:7605403899"><b>760 540 3899</b></a></div>
+  <a href="#" class="c-button c-button--primary c-header__cta">Make an appointment</a>
 </nav>
