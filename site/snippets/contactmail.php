d3ab59dd9637206fd0147e158ade390e9ff9ec67
Julio Cesar Montenegro <creativo@686studio.com>
2017-08-09 12:37:29 -0700

    template email

--- site/snippets/contactmail.php
+++ site/snippets/contactmail.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Email</title>
-</head>
-<body>
-	You have received a new message, here are the details:
-	<br>
-	<b>Name:</b> <?= $fullName ?><br>
-	<b>Email:</b> <?= $emailAddress ?><br>
-	<b>City of residence:</b> <?= $cityOfResidence ?><br>
-	<b>Contact number:</b> <?= $contactNumber ?><br>
-	<b>Message:</b><br> <?= $message ?>
-</body>
-</html>
+<?= '<b>'.'You have received a new message, here are the details:'.'</b>' ?>
+Name: <?= $fullName ?>
+Email: <?= $emailAddress ?>
+City of residence: <?= $cityOfResidence ?>
+Contact number: <?= $contactNumber ?>
+Message: <?= $message ?>
+
+
